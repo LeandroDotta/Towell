0ed858eddb30eea55d1544c763cbfad1525dd130
Leandro Dotta <leandrodotta@gmail.com>
2024-09-21 01:33:54 -0300

    fix include directories in premake

--- premake5.lua
+++ premake5.lua
@@ -13,6 +13,29 @@ workspace "Towell"
 		"Release" 
 	}
 
+	externalincludedirs 
+	{
+		"%{extDir.SDL2}/include",
+		"%{extDir.glew}/include",
+		"%{extDir.SOIL2}/src/SOIL2",
+	}
+
+	libdirs
+	{
+		"%{extDir.SDL2}/lib/x64",
+		"%{extDir.glew}/lib/Release/x64",
+		"%{extDir.SOIL2}/lib/windows",
+	}
+
+	links
+	{
+		"SDL2",
+		"SDL2main",
+		"opengl32",
+		"glew32",
+		"soil2-debug.lib",
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "Engine"
