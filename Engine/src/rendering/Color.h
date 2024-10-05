b3bc52eede4456b0338d8651ad47af30fea02603
Leandro Dotta <leandrodotta@gmail.com>
2024-10-05 14:42:04 -0300

    create a Color struct to handle RGB colors

---
+++ Engine/src/rendering/Color.h
@@ -0,0 +1,31 @@
+#ifndef TOWELL_COLOR_H
+#define TOWELL_COLOR_H
+
+namespace Towell
+{
+	struct Color
+	{
+	public:
+		float r;
+		float g;
+		float b;
+		float a;
+
+		Color();
+		Color(float r, float g, float b);
+		Color(float r, float g, float b, float a);
+		~Color();
+
+		float* ToFloatArray() const;
+
+		static const Color White;
+		static const Color Black;
+		static const Color Red;
+		static const Color Green;
+		static const Color Blue;
+		static const Color Transparent;
+	};
+}
+
+#endif // TOWELL_COLOR_H
+
